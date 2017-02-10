56637060037634c1bb7c695800bd50f7b3dc45a7
Jonathan Mancia <jonathanman2010@icloud.com>
2017-02-09 19:01:12 -0700

    Added mongoose, connected to db

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/notes');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var notes = require('./routes/notes');
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('notes', notes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
